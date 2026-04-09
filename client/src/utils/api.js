1ef6943fcb11b6fa9d4d8e109d4c85f91e644991
Anjef Dangol <njrnjf@gmail.com>
2026-04-09 12:59:24 +0545

    API restructured

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  // Use environment variable, otherwise fallback to localhost for dev
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000', 
 });
 
 export default API;
